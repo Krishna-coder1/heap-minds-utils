73b91ff085d3d1c773c977cfb28d2ed753bd4109
Krishna-coder1 <teja.krishna.akp@gmail.com>
2023-11-28 15:30:53 +0530

    changed package name

--- src/main/java/org/heapminds/ro/BaseSro.java
+++ src/main/java/org/heapminds/ro/BaseSro.java
@@ -1,17 +1,16 @@
 package org.heapminds.ro;
 
-
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class BaseSro<T> {
-    public BaseSro(T data){
+    public BaseSro(T data) {
+        System.out.println(data);
         this.data = data;
     }
 
-    private Boolean success=true;
+    private Boolean success = true;
     private T data;
 }
